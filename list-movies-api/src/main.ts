2430139953d0869865463a6ae1e086e4071ad394
git <ignaciosotelo8@gmail.com>
2024-04-17 02:11:08 -0300

    trying cors

--- list-movies-api/src/main.ts
+++ list-movies-api/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const ORIGIN_URL = ['http://localhost:3000', 'http://localhost:5173']
+  const ORIGIN_URL = ['http://localhost:3000', 'http://localhost:5173', 'https://ignaciosotelo5.github.io/movies-app-deploy/' ]
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ORIGIN_URL
